fc548e846e856e1843167b17b7ce2b947340e3f3
BENG <quinitamj30@gmail.com>
2026-05-14 01:56:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(__dirname));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
