705e1e5b24112e645a0fca0b5ae0caa67b5ab361
BENG <quinitamj30@gmail.com>
2026-05-14 01:46:30 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// Mas mainam gamitin ang connectionString para sa Render
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Importante: Pinapayagan nito ang secure connection sa Render
+    }
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the PostgreSQL database');
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
 });
 
 module.exports = pool;
